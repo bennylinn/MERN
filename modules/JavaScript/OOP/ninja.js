879d38d103ea14614ad97dc98ef577c6b51593b5
Benny Lin <bennylin1995@gmail.com>
2020-09-01 16:02:36 -0700

    ninjas

--- modules/JavaScript/OOP/ninja.js
+++ modules/JavaScript/OOP/ninja.js
@@ -11,6 +11,7 @@ class Ninja {
     }
 
     showStats() {
+        console.log("I am", this.name);
         console.log("strength:", this.strength);
         console.log("speed:", this.speed);
         console.log("health:", this.health);
@@ -20,3 +21,29 @@ class Ninja {
         this.health += 10;
     }
 }
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);
+        this.speed = 10;
+        this.strength = 10;
+        this.widsdom = 10;
+    }
+
+    speakWisdom() {
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+        super.drinkSake()
+    }
+
+    showStats() {
+        super.showStats();
+        console.log("wisdom:", this.widsdom);
+    }
+}
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
