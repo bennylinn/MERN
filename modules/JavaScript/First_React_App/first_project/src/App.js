9679bf0b6af220474d57f60f91a80468d1521a18
Benny Lin <bennylin1995@gmail.com>
2020-09-04 15:11:42 -0700

    first jsx

---
+++ modules/JavaScript/First_React_App/first_project/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {    
+  render() {        
+      return (
+        <div>
+          <h1>Hello Dojo</h1>
+          <h2>Things I need to do:</h2>
+          <li>Learn React</li>
+          <li>Learn React</li>
+          <li>Learn React</li>
+        </div>
+      );    
+  }
+}
+
+export default App;
